cd60503ff114453545752f6c14278058d204731c
Ryan Kretz <rkretz@lion.lmu.edu>
2014-11-17 17:44:26 -0800

    Submission of Homework5

--- homework5/src/main/java/edu/lmu/cmsi/ryan/homework5/RecursiveStringToInt.java
+++ homework5/src/main/java/edu/lmu/cmsi/ryan/homework5/RecursiveStringToInt.java
@@ -1,18 +1,5 @@
 package edu.lmu.cmsi.ryan.homework5;
 
-/*public class RecursiveStringToInt {
-
-	public static int StringToInt(String s) {
-		if (s.length() == 0) {
-			return 0;
-		} else if (s.length() == 1) {
-			return Integer.parseInt(s);
-		} else {
-			return (StringToInt(s.substring(0,s.length() - 2)) * 10) + Integer.parseInt(s.substring(s.length() - 1, s.length()));
-		}
-	}
-}*/
-
 public class RecursiveStringToInt {
 
 	public static int StringToInt(String s) {
