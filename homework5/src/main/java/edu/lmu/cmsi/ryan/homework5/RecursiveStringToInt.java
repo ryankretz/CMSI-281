33386a93a9f85948b51add9c6182cfcf429c6839
Ryan Kretz <rkretz@lion.lmu.edu>
2014-11-17 17:43:58 -0800

    Submission of Homework5

--- homework5/src/main/java/edu/lmu/cmsi/ryan/homework5/RecursiveStringToInt.java
+++ homework5/src/main/java/edu/lmu/cmsi/ryan/homework5/RecursiveStringToInt.java
@@ -1,15 +1,28 @@
 package edu.lmu.cmsi.ryan.homework5;
 
-public class RecursiveStringToInt {
-	static int num = 0;
-	static int index  = 0;
+/*public class RecursiveStringToInt {
 
 	public static int StringToInt(String s) {
-		if (s.length() == 1) {
-			return (num * 10) + Integer.parseInt(s.substring(0,1));
+		if (s.length() == 0) {
+			return 0;
+		} else if (s.length() == 1) {
+			return Integer.parseInt(s);
 		} else {
-			num = (num * 10) + Integer.parseInt(s.substring(0,1));
-			return StringToInt(s.substring(1, s.length()));
+			return (StringToInt(s.substring(0,s.length() - 2)) * 10) + Integer.parseInt(s.substring(s.length() - 1, s.length()));
+		}
+	}
+}*/
+
+public class RecursiveStringToInt {
+
+	public static int StringToInt(String s) {
+		int num = s.length();
+		if (s.isEmpty()) {
+			return 0; //if string is empty, return 0
+		} else if(s.length() == 1) {
+			return Integer.parseInt(s); //convert single character to number
+		} else {
+			return StringToInt(s.substring(num - 1, num)) + StringToInt(s.substring(0, num - 1)) * 10;
 		}
 	}
 }
--- homework5/src/test/java/edu/lmu/cmsi/ryan/homework5/RecursiveStringToIntTest.java
+++ homework5/src/test/java/edu/lmu/cmsi/ryan/homework5/RecursiveStringToIntTest.java
@@ -13,10 +13,12 @@ public class RecursiveStringToIntTest {
 		assertEquals("failure - did not convert big string to integer", 89753, RecursiveStringToInt.StringToInt(bigString));
 	}
 
-	@Test   //convert small string to int
+	@Test   //convert small string to int. Also tests if string is empty
 	public void smallStringToIntTest() {
 		String smallString = "123";
+		String emptyString = "";
 		assertEquals("failure - did not convert small string to integer", 123, RecursiveStringToInt.StringToInt(smallString));
+		assertEquals("failure - did not convert empty string to 0", 0, RecursiveStringToInt.StringToInt(emptyString));
 	}
 
 
